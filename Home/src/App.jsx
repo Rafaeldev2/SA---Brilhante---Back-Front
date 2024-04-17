2a5dabd57d00c2da035296c09e729e951088da87
João Paulo Ferreira Leite <114254411+JoaoPauloSenai@users.noreply.github.com>
2024-04-17 17:37:53 -0300

    Atualização-Tela-de-Login17/04/2024
    Atualização Tela de Login 17/04/2024.
    Criação da tela de login.

--- Home/src/App.jsx
+++ Home/src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import Card from './assets/Components/Card'
+import LoginForm from './LoginForm'
 
 function App() {
 
   return (       
-    <div className='container'>
-        <h1>Bijuteria</h1>
-        <div className="card-grid">
-            <Card/>
-            <Card/>
-        </div>
-    </div>
+    // <div className='container'>
+    //     <h1>Bijuteria</h1>
+    //     <div className="card-grid">
+    //         <Card/>
+    //         <Card/>
+    //     </div>
+    // </div>
+    <LoginForm/>
   )
 }
 
---
+++ Home/src/LoginForm.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar lógica para autenticar o usuário
+    console.log('Email:', email);
+    console.log('Senha:', password);
+  };
+
+  return (
+    <div className='background-container'>
+      <div className="login-container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Senha:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <button type="submit">Confirmar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
