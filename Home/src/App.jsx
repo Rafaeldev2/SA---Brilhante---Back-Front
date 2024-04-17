292a7a2936d5ae9e5f7dbf629ec7b812f87c78de
Rafaeldev2 <116116255+Rafaeldev2@users.noreply.github.com>
2024-04-17 15:14:16 -0300

    Commit inicial SA

---
+++ Home/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './assets/Components/Card'
+
+function App() {
+
+  return (       
+    <div className='container'>
+        <h1>Bijuteria</h1>
+        <div className="card-grid">
+            <Card/>
+            <Card/>
+        </div>
+    </div>
+  )
+}
+
+export default App
