67f54f1b16dff5033861ad4e70c52016db607559
João Paulo Ferreira Leite <114254411+JoaoPauloSenai@users.noreply.github.com>
2024-05-24 17:36:55 -0300

    footer feito em sala

--- Brilhante/src/Components/Footer.jsx
+++ Brilhante/src/Components/Footer.jsx
@@ -1,10 +1,37 @@
+import React, { useState } from 'react';
+import './Footer.css'
+import { Link } from 'react-router-dom';
 
 function Footer() {
+
     return (
-        <div>
-            <p>Footer</p>
-        </div>
-    )
-}
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="footer-about-us">
+                    <h4>Sobre Nós</h4>
+                    <p>Bijuterias exclusivas e de alta qualidade para todos os estilos.</p>
+                </div>
+                <div className="footer-contact">
+                    <h4>Contato</h4>
+                    <p>Email: Brilhante@bijuterias.com</p>
+                    <p>Telefone: (48) 9 1234-5678</p>
+                </div>
+                <div className="footer-icon">
+                    <h4>Siga-nos</h4>
+                    <div className="social-icons">
+                        <img className='Icon' src="./img/insta.png" />
+                        <img className='Icon' src="./img/twitter.png" />
+                        <img className='Icon' src="./img/facebook.png" />
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2024 Brilhante. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
+};
+
 
 export default Footer;
+
--- Brilhante/src/Pages/LoginForm.jsx
+++ Brilhante/src/Pages/LoginForm.jsx
@@ -2,50 +2,51 @@ import React, { useState } from 'react';
 import './LoginForm.css';
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+  // const handleEmailChange = (event) => {
+  //   setEmail(event.target.value);
+  // };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  // const handlePasswordChange = (event) => {
+  //   setPassword(event.target.value);
+  // };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Aqui você pode adicionar lógica para autenticar o usuário
-    console.log('Email:', email);
-    console.log('Senha:', password);
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   //lógica para autenticar o usuário
+  //   console.log('Email:', email);
+  //   console.log('Senha:', password);
+  // };
 
   return (
     <>
       <div className='background-container'>
         <div className="login-container">
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="input-group">
-              <label>Email:</label>
-              <input
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                required
-              />
-            </div>
-            <div className="input-group">
-              <label>Senha:</label>
-              <input
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-              />
-            </div>
-            <button type="submit">Confirmar</button>
-          </form>
+          <div>
+            <h2>Login</h2>
+          </div>
+          {/* <form onSubmit={handleSubmit}> */}
+          
+            <input
+              className="custom-input"
+              placeholder='Email:'
+              type="email"
+            // value={email}
+            // onChange={handleEmailChange}
+            // required
+            />        
+            <input
+              className="custom-input"
+              placeholder='Senha:'
+              type="password"
+            // value={password}
+            // onChange={handlePasswordChange} 
+            // required
+            />
+          <button type="submit">Confirmar</button>
+          {/* </form> */}
         </div>
       </div>
     </>
