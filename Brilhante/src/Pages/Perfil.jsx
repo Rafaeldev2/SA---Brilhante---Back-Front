db853dfa8da7536fc5bea15247afc4f6bd3f54e3
João Paulo Ferreira Leite <114254411+JoaoPauloSenai@users.noreply.github.com>
2024-06-05 17:49:23 -0300

    2° att 06/06/2024

--- Brilhante/src/Pages/Perfil.jsx
+++ Brilhante/src/Pages/Perfil.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-
+// import './Perfil.css';
 
 const Perfil = () => {
-  return (
-    <>
-      <h2>Perfil</h2>
-    </>
-  );
-};
 
+  return (
+    <div className="profile-container">
+      <h2>Perfil do Usuário</h2>
+      <p>Nome: João da Silva</p>
+      <p>CPF: 123.456.789-00</p>
+      <p>E-mail: joao.paulo@hotmail.com</p>
+    </div>
+  );
+}
 
 export default Perfil;
 
