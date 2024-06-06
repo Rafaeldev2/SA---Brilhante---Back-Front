47c2e34569efe87ac8c1ad0611c07098bf12e34b
lucas <109703831+lucasmv6@users.noreply.github.com>
2024-06-06 17:21:50 -0300

    Tela de Listar Produto

--- Brilhante/src/Pages/ListaProdutos.jsx
+++ Brilhante/src/Pages/ListaProdutos.jsx
@@ -2,49 +2,77 @@ import React, { useState, useEffect } from 'react';
 import './ListaProdutos.css';
 
 const ListaProdutos = () => {
-const [products, setProdutos] = useState([]);
+  const [products, setProdutos] = useState([]);
 
-useEffect(() => {
-const fetchProdutos = async () => {
+  useEffect(() => {
+    const fetchProdutos = async () => {
 
-const productData = [
-{ id: 1, name: 'Produto A', price: 10.0, quantity: 5, barcode: 57363833 },
-{ id: 2, name: 'Produto B', price: 20.0, quantity: 3, barcode: 95738392 },
-{ id: 3, name: 'Produto C', price: 30.0, quantity: 8, barcode: 12345678},
-];
-setProdutos(productData);
-};
+      const productData = [
+        { id: 1, name: 'Produto A', price: 10.0, quantity: 5, barcode: 57363833 },
+        { id: 2, name: 'Produto B', price: 20.0, quantity: 3, barcode: 95738392 },
+        { id: 3, name: 'Produto C', price: 30.0, quantity: 8, barcode: 12345678 },
+      ];
+      setProdutos(productData);
+    };
 
-  fetchProdutos();
+    fetchProdutos();
   }, []);
   return (
-    <div className='container'>
+    <div className='container-list'>
       <h2>Lista de Produtos</h2>
-      <table>
-        <thead>
-          <tr className='titulos'>
+      <table className='list-product'>
+          <div className='valores-lista'>
+            <div className='div-id' >
             <th className='ID'>ID</th>
+              <tl>1</tl>
+              <tl>2</tl>
+              <tl>3</tl>
+              <tl>4</tl>
+              <tl>5</tl>
+              <td className='id' >{ }</td>
+            </div>
+            <div className='div-id' >
             <th className='Nome'>Nome</th>
+              <tl>.50</tl>
+              <tl>P350</tl>
+              <tl>MAGNUM</tl>
+              <tl>RIFLE DE CAÇA</tl>
+              <tl>.40</tl>
+              <td className='name'>{ }</td>
+            </div>
+            <div className='div-id' >
             <th className='Preço'>Preço</th>
+              <tl>898</tl>
+              <tl>747</tl>
+              <tl>546</tl>
+              <tl>4554</tl>
+              <tl>43435</tl>
+              <td className='price'>{ }</td>
+            </div>
+            <div className='div-id' >
             <th className='Quantidade'>Quantidade</th>
+              <tl>94359</tl>
+              <tl>354543262</tl>
+              <tl>355452</tl>
+              <tl>457435</tl>
+              <tl>45435</tl>
+              <td className='quantity'>{ }</td>
+            </div>
+            <div className='div-id' >
             <th className='Codigo-de-barra'>Codigo de barra</th>
-          </tr>
-        </thead>
-        <tbody className='valores'>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td className='id' >{product.id}</td>
-              <td className='name'>{product.name}</td>
-              <td className='price'>{product.price}</td>
-              <td className='quantity'>{product.quantity}</td>
-              <td className='barcode'>{product.barcode}</td>
-              </tr>
-            ))}
-        </tbody>
+              <tl>57363833</tl>
+              <tl>95738392</tl>
+              <tl>12345678</tl>
+              <tl>95823723</tl>
+              <tl>76528295</tl>
+              <td className='barcode'>{ }</td>
+            </div>
+          </div>
+
       </table>
     </div>
   );
-    
+
 };
 
 export default ListaProdutos;
