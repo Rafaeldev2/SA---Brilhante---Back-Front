6588386462ba6e54dbe50c2d8f50674858511294
João Paulo Ferreira Leite <114254411+JoaoPauloSenai@users.noreply.github.com>
2024-06-07 19:33:42 -0300

    Mega atualização 07/06/2024

--- Brilhante/src/Context/GlobalContext.jsx
+++ Brilhante/src/Context/GlobalContext.jsx
@@ -6,8 +6,17 @@ export const BrilhanteContext = createContext()
 export const BrilhanteContextProvider = (({children}) => {
     const [email, setEmail] = useState([])
 
+    const [products, setProducts] = useState([
+        { id: 1, name: 'Anel com firulas', tipo: 'Anel', description: 'Descrição do produto', price: 15, quantity: 1 },
+        { id: 2, name: 'Brico de uvas', tipo: 'Brinco', description: 'Descrição do produto', price: 25, quantity: 1 },
+        { id: 3, name: 'Colar de perolas', tipo: 'Colar', description: 'Descrição do produto', price: 30, quantity: 1 },
+        { id: 4, name: 'Conjunto Persa', tipo: 'Conjunto', description: 'Descrição do produto', price: 40, quantity: 1 },
+        { id: 5, name: 'Misanga arco iris', tipo: 'Pulseira', description: 'Descrição do produto', price: 5, quantity: 1 },
+        // Adicione mais produtos conforme necessário
+      ]);
+
     return (
-        <BrilhanteContext.Provider value={{email, setEmail}}>
+        <BrilhanteContext.Provider value={{email, setEmail, products, setProducts}}>
             {children}
         </BrilhanteContext.Provider>
     )
--- Brilhante/src/Pages/HomePage.jsx
+++ Brilhante/src/Pages/HomePage.jsx
@@ -1,76 +1,49 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import {BrilhanteContext} from "../Context/GlobalContext"
 import './HomePage.css';
 
 function HomePage() {
+
+  const {products, setProducts} = useContext(BrilhanteContext);
+
+  const handleQuantityChange = (index, event) => {
+    const newProducts = [...products];
+    newProducts[index].quantity = parseInt(event.target.value);
+    setProducts(newProducts);
+  };
+
   return (
     <>
-      <div className='container'>
-        <div className='div-card-produto'>
-          <h1>Nossas Bijuterias</h1>
-          <img className='img-card' src="./img/Aneis/Anel-com-firulas.png" />
-          <h2>Nome do Produto</h2>
-          <p>Descrição do produto</p>
-          <div className="div-card-price-cart">
-            <p className="price">R$ 25,00</p>
-            <button className="cart-button" ><img src="./img/Cart.png" className="card-icon" />
-            </button>
-          </div>
-        </div>
-        <div className='div-card-produto'>
-          <h1>Nossas Bijuterias</h1>
-          <img className='img-card' src="./img/Brincos/Brico-de-uvas.png" />
-          <h2>Nome do Produto</h2>
-          <p>Descrição do produto</p>
-          <div className="div-card-price-cart">
-            <p className="price">R$ 25,00</p>
-            <button className="cart-button" ><img src="./img/Cart.png" className="card-icon" />
-            </button>
-          </div>
-        </div>
-        <div className='div-card-produto'>
-          <h1>Nossas Bijuterias</h1>
-          <img className='img-card' src="./img/Colares/Colar-de-perolas.jpg" />
-          <h2>Nome do Produto</h2>
-          <p>Descrição do produto</p>
-          <div className="div-card-price-cart">
-            <p className="price">R$ 25,00</p>
-            <button className="cart-button" ><img src="./img/Cart.png" className="card-icon" />
-            </button>
-          </div>
-        </div>
-        <div className='div-card-produto'>
-          <h1>Nossas Bijuterias</h1>
-          <img className='img-card' src="./img/Conjunto/Conjunto-de-colar-e-brincos-Persa.png" />
-          <h2>Nome do Produto</h2>
-          <p>Descrição do produto</p>
-          <div className="div-card-price-cart">
-            <p className="price">R$ 25,00</p>
-            <button className="cart-button" ><img src="./img/Cart.png" className="card-icon" />
-            </button>
-          </div>
-        </div>
-        <div className='div-card-produto'>
-          <h1>Nossas Bijuterias</h1>
-          <img className='img-card' src="./img/Pulseiras/Misanga-arco-iris.jpg" />
-          <h2>Nome do Produto</h2>
-          <p>Descrição do produto</p>
-          <div className="div-card-price-cart">
-            <p className="price">R$ 25,00</p>
-            <button className="cart-button" ><img src="./img/Cart.png" className="card-icon" />
-            </button>
-          </div>
-        </div>
-        <div className='div-card-produto'>
-          <h1>Nossas Bijuterias</h1>
-          <img className='img-card' src="./img/Aneis/Anel-dourado.png" />
-          <h2>Nome do Produto</h2>
-          <p>Descrição do produto</p>
-          <div className="div-card-price-cart">
-            <p className="price">R$ 25,00</p>
-            <button className="cart-button" ><img src="./img/Cart.png" className="card-icon" />
-            </button>
-          </div>
+      <div className='home-container'>
+        <h1>Home Page</h1>
+        <div className='div-container'>
+          {products.map((product, index) => (
+            <div className='div-card-produto' key={product.id}>
+              <img className='img-card' src={`./img/${product.tipo}/${product.name}.png`} alt={product.name} />
+              <h4>{product.name}</h4>
+              <p>{product.description}</p>
+              <div className="quantity-container">
+              <p className="price">R$ {product.price.toFixed(2)}</p>
+                <label htmlFor={`quantity-${product.id}`}>Quantidade:</label>
+                <input
+                  type="number"
+                  id={`quantity-${product.id}`}
+                  name={`quantity-${product.id}`}
+                  min="1"
+                  value={product.quantity}
+                  onChange={(e) => handleQuantityChange(index, e)}
+                />
+              </div>
+              <div className="div-card-price-cart">
+
+                <p className="total-price">Valor Total: R$ {(product.price * product.quantity).toFixed(2)}</p>
+              </div>
+              <div className="div-card-price-cart">
+                <button className="button-add-to-cart">Adicionar ao carrinho</button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
