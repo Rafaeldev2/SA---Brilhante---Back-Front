e4d532c46f475ccce58aec68830b0a40a39d5ca7
João Paulo Ferreira Leite <114254411+JoaoPauloSenai@users.noreply.github.com>
2024-06-11 15:19:19 -0300

    att 2

--- Brilhante/src/Components/Footer.jsx
+++ Brilhante/src/Components/Footer.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 import { Link } from 'react-router-dom';
 
 function Footer() {
-
     return (
-        <footer className="footer">
-            <div className="footer-container">
+        <div className="footer-container">
+            <div className='footer-content'>
                 <div className="footer-about-us">
                     <h4>Sobre Nós</h4>
                     <p>Bijuterias exclusivas e de alta qualidade para todos os estilos.</p>
@@ -16,22 +15,22 @@ function Footer() {
                     <p>Email: Brilhante@bijuterias.com</p>
                     <p>Telefone: (48) 9 1234-5678</p>
                 </div>
-                <div className="footer-icon">
+                <div className="footer-Icon">
                     <h4>Siga-nos</h4>
-                    <div className="social-icons">
-                        <img className='Icon' src="./img/insta.png" />
-                        <img className='Icon' src="./img/twitter.png" />
-                        <img className='Icon' src="./img/facebook.png" />
+                    <div className="Icons">
+                        <img className='Icon' src="./img/insta.png" alt="Instagram" />
+                        <img className='Icon' src="./img/twitter.png" alt="Twitter" />
+                        <img className='Icon' src="./img/facebook.png" alt="Facebook" />
                     </div>
                 </div>
             </div>
-            <div className="footer-bottom">
-                <p>&copy; 2024 Brilhante. Todos os direitos reservados.</p>
+            <div className='footer-bottom-container'>
+                <div className="footer-bottom">
+                    <p>&copy; 2024 Brilhante. Todos os direitos reservados.</p>
+                </div>
             </div>
-        </footer>
+        </div>
     );
-};
-
+}
 
 export default Footer;
-
